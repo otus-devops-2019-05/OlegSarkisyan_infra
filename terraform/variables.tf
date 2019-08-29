b5b9b224963f08b135adb293e13df7ba20f0ef43
OlegSarkisyan <oleg.sarkisyan@homecredit.ru>
2019-08-29 07:40:19 +0000

    7 Homework

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,27 +5,32 @@ variable project {
 variable region {
   description = "Region"
 
-  # Значение по умолчанию
   default = "europe-west1"
+}
+
+variable public_key_path {
+  description = "Path to the public key used for ssh access"
+}
+
+variable disk_image {
+  description = "disk image"
+}
+
+variable private_key_pach {
+  description = "private key appuser"
 }
 
 variable zone {
   description = "Zone"
-
-  # Значение по умолчанию
-  default = "europe-west1-b"
+  default     = "europe-west1-b"
 }
 
-variable public_key_path {
-  # Описание переменной
-  description = "Path to the public key used for ssh access"
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
 }
 
-variable private_key_path {
-  # Описание переменной
-  description = "Path to the private key used for ssh access"
-}
-
-variable disk_image {
-  description = "Disk image"
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
 }
