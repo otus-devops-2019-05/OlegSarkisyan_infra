a9c8d067a6119a7a90777493ad38768d9ae09bc2
OlegSarkisyan <oleg.sarkisyan@homecredit.ru>
2019-08-29 13:24:59 +0000

    7.7 HW

--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -21,10 +21,6 @@ variable public_key_path {
   description = "Path to the public key used for ssh access"
 }
 
-variable private_key_pach {
-  # Описание переменной
-  description = "Path to the private key used for ssh access"
-}
 
 variable disk_image {
   description = "Disk image"
